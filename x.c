91c92f53a831ceb1c8943ab23980e40295542f16
Janey <1737941060@qq.com>
2022-04-13 22:29:32 +0800

    rightclickpaste

--- x.c
+++ x.c
@@ -712,6 +712,10 @@ brelease(XEvent *e)
 		mousereport(e);
 		return;
 	}
+	if (e->xbutton.button == Button3)
+ 		selpaste(NULL);
+ 	else if (e->xbutton.button == Button1)
+ 		mousesel(e, 1);
 
 	if (mouseaction(e, 1))
 		return;
